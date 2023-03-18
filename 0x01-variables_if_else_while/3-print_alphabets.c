205e4b4b188b8238fca31f86c832b927d774c5f3
Alan <abogonono1@gmail.com>
2023-03-18 12:22:50 -0700

    A script that print the alphabet in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Already (success)
+ *
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 49; i < 75; i++)
+	{
+		printf("%c", i + '0');
+	}
+	printf("\n");
+	for (i = 17; i < 43; ++)
+	{
+		printf("%c", i + '0');
+	}
+	printf("\n");
+	return (0);
+}
