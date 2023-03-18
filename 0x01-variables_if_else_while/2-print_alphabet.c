38e51339420ceb784638f97a3921c19573938eaf
Alan <abogonono1@gmail.com>
2023-03-18 11:52:04 -0700

    a script that print the alphbet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return 0 Already (success)
+ */
+int main(void)
+{
+	int i = 49;
+	for(;i < 75; i++)
+	{
+		printf("%c\n", i + '0');
+	}
+	return (0);
+}
