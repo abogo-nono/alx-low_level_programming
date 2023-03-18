212384153b6e75e41fe202ddabd187e52b2a58fe
Alan <abogonono1@gmail.com>
2023-03-18 12:03:00 -0700

    A script that print the alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- *
  * Return 0 Already (success)
+ *
  */
 int main(void)
 {
-	int i = 49;
-	for(;i < 75; i++)
+
+	int i;
+
+	for (i = 49; i < 75; i++)
 	{
 		printf("%c\n", i + '0');
 	}
