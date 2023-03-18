e9e638545b17272b8dd792de2653db9cc4716f9e
Alan <abogonono1@gmail.com>
2023-03-18 13:44:55 -0700

    A script that print all the digits of the base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Already (success)
+ *
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
