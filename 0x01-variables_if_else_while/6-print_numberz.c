8564e7354e9889c34014732aa12bf7eaf757d807
Alan <abogonono1@gmail.com>
2023-03-18 13:54:13 -0700

    A script that print all digits of the base 10 only using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Already (success)
+ *
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0')
+	}
+	printf("\n");
+	return (0);
+}
