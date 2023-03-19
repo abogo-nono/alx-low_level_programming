83b416c52a5aacea30f859867f20b99f5347b342
Alan <abogonono1@gmail.com>
2023-03-18 17:14:28 -0700

    A script that print all single digit of a number

---
+++ 0x01-variables_if_else_while/.9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Already (success)
+ *
+ */
+int main(void)
+{
+	int number, i;
+	char number_letter[50]; 
+ 
+	sprintf(number_letter, "%d", number);	
+	for (i = 0; i < strlen(number_letter); i++)
+	{
+		putchar(number_letter[i]);
+		putchar(',');
+		putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
