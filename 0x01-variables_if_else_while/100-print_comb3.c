96b79728da7ae8caff11ff53f3e492ff0401c287
Alan <abogonono1@gmail.com>
2023-03-18 20:01:15 -0700

    a script that print the combination 2 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Already (success)
+ *
+ */
+int main(void)
+{
+	int i, j, digits[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 8 && j != 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	printf("\n");
+	return (0);
+}
