e53ed3278fc8d7c70eaa237cf00df9d3de4eb707
Alan <abogonono1@gmail.com>
2023-03-27 18:32:40 -0700

    character counter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - count the numberf characters of a string
+ * @s: the string
+ * Return: i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
+	return (i);
+}
+
