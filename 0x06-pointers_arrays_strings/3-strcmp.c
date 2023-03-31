825b44f2cfdf8457d178c63f3274df955a3bc1f3
Alan <abogonono1@gmail.com>
2023-03-31 16:51:45 -0700

    A function that compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *dest, char *src)
 {
 	int i, n = strlen(dest);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && dest[i] != '\0'; i++)
 		if ((int)dest[i] < (int)src[i])
 			return (-15);
 		else if ((int)dest[i] > (int)src[i])
