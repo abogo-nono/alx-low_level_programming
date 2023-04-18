6dbd2d79bacf8a518663af3b2fbaa7a9fecdf7b4
Alan <abogonono1@gmail.com>
2023-04-17 17:11:31 -0700

    A structure to define the dog type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-typedef struct dog {
-	char* name;
-	int age;
-	char* owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - used to represent a dog
+ * @name: dog name
+ * @age: og age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
+typedef struct dog;
+
+#endif
