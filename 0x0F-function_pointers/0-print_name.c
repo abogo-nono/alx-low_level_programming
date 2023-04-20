8f8bfc01cde1fd6d98ace1c701d3423e6a65a1cd
Alan <abogonono1@gmail.com>
2023-04-19 18:09:07 -0700

    a pointer function used to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointer.h"
+
+/**
+ * print_name - This function is used to print a name passed as param
+ * @name: the passed name
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name)
+	{
+		f(name);
+	}
+}
