c9e85a62f96b0ed999f1be94a756654392129bb3
Alan <abogonono1@gmail.com>
2023-04-20 18:14:54 -0700

    print params

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - this function is used to print his params.
+ * @separator: params seprator.
+ * @n: number of params
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	const char *s = separator;
+	va_list l;
+
+	va_start(l, n);
+	for (i = 0; i < n; i++)
+	{
+		s = (i < (n - 1) ? separator : NULL);
+		printf("%d", va_arg(m, int));
+		if (s != NULL)
+		printf("%s", s);
+	}
+	printf("\n");
+}
