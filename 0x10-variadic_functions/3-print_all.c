154e3ecbf8871925a1953fbcaf521102082e6aaf
Alan <abogonono1@gmail.com>
2023-04-20 18:37:35 -0700

    print all param

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints all type of params.
+ * @format: param format.
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0, len = 0;
+	char *str;
+	va_list l;
+
+	va_start(l, format);
+	while (format[len])
+		len++;
+	while (i < len)
+	{
+		switch (format[i])
+		{
+			case 'c':
+			printf("%c", va_arg(l, int));
+			break;
+			case 'i':
+			 printf("%d", va_arg(l, int));
+			break;
+			case 's':
+			str = va_arg(l, char *);
+			printf("%s", (str != NULL) ? str : "(nil)");
+			break;
+			case 'f':
+			printf("%f", va_arg(m, double));
+			break;
+			default:
+			exit(1);
+			break;
+		}
+		i++;
+		if (i < (len))
+			printf(", ");
+	}
+	va_end(l);
+	printf("\n");
+}
