65247459e63d1a3e757647db2610d2ad7d470f28
abogo-nono <abogonono1@gmail.com>
2023-07-25 11:50:48 -0700

    a function that prints the alphabet, in lowercase 10 time, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that print alphabet in lowercase 10 time
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+        	_putchar('\n');
+	}
+
+}
