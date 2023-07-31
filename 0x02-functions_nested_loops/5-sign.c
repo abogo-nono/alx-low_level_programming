7c7edff0ad1a77611e463618e0952b128d41b013
abogo-nono <abogonono1@gmail.com>
2023-07-31 12:23:23 -0700

    a function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+/**
+ * print_sign - A function that prints the sign of a number
+ *
+ * @n: the number
+ *
+ * Return: 1 if n > 0, 0 if  n = 0 and -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+        if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+}
