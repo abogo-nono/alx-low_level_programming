36000abca1f09b3f45884a2c037c6c8f311fe54b
abogo-nono <abogonono1@gmail.com>
2023-07-31 13:50:42 -0700

    a function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+/**
+ * print_last_digit - a function that prints the last digit of a number.
+ * @n: the number
+ * Return: the last digit of a number
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
