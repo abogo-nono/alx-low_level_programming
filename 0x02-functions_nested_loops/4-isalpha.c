28676d19d26b2978c53f8ba6b4ef32ff5bc42f13
abogo-nono <abogonono1@gmail.com>
2023-07-31 10:56:29 -0700

    a function that check for alpha char

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+/**
+ * _isalpha - A function that checks for alphabet character
+ *
+ * @c: the char to check
+ *
+ * Return: 1 if it's an alphabetic char and 0 else
+ */
+int _isalpha(int c)
+{
+	int i, j;
+
+	for (i = 97; i < 123; i++)
+		if (c == i)
+			return (1);
+
+	for (j = 65; j < 91; j++)
+		if(c == j)
+			return (1);
+
+	return (0);
+}
