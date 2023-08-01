6da9506b5bdd934001b81c04c04f03384915c428
abogo-nono <abogonono1@gmail.com>
2023-08-01 17:32:10 +0100

    a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - A function that prints the 9 times table, starting with 9.
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int result = i * j;
+
+			if (result > 9)
+				_putchar(result / 10 + '0');
+
+			_putchar(result % 10 + '0');
+
+			if (result != 81)
+			{
+				_putchar(',');
+				if (result <= 9)
+					_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,30 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			int result = i * j;
-
-			if (result > 9)
-				_putchar(result / 10 + '0');
-
-			_putchar(result % 10 + '0');
-
-			if (result != 81)
+			if ((result / 10) == 0)
 			{
-				_putchar(',');
-				if (result <= 9)
+				if (j == 0)
+					_putchar('0');
+				if (j != 0)
+				{
 					_putchar(' ');
-				_putchar(' ');
+					_putchar((result % 10) + '0');
+				}
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 		_putchar('\n');
