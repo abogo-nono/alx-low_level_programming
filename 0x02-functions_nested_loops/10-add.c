1e095c9714c53a8443fc910c60adee0ccdf528fb
abogo-nono <abogonono1@gmail.com>
2023-08-01 05:35:14 +0100

    a function that add two number and return the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - A function thaat add to number and return result
+ * @n1: the first number
+ * @n2: the second number
+ * Return: the sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
