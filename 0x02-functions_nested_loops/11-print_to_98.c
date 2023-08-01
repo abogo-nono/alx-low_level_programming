1ecaf7b7dbbc84c694a123d0140b09a7bb3d3c3a
abogo-nono <abogonono1@gmail.com>
2023-08-01 17:05:38 +0100

    a function that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - a function that prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - a function that prints all natural numbers from n to 98
  * @n: the number passed
  * Return: void
  */
