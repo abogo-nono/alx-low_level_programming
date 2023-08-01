7c3e5c40c02f02d691d3995dc10ae24e72370ee0
abogo-nono <abogonono1@gmail.com>
2023-08-01 23:07:52 +0100

    A function that print the sum of all multiple of 3 and 5 belo 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,9 +7,9 @@
  */
 void sum_of_multiple_of_3_or_5_blow_1024(void)
 {
-	int s = 0;
+	int s = 0, i;
 
-	for (int i = 0; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 		if (i % 3 == 0 || i % 5 == 0)
 			s += i;
 	printf("%d\n", s);
