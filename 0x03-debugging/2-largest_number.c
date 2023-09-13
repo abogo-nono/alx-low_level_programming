fb78b3bd0dc5db977649075963ca085f5cd0e1c6
abogo-nono <abogonono1@gmail.com>
2023-09-14 00:39:54 +0100

    modified:   0x03-debugging/1-main.c 	modified:   0x03-debugging/2-largest_number.c 	new file:   0x03-debugging/2-main.c 	new file:   0x03-debugging/3-convert_day.c 	new file:   0x03-debugging/3-main_a.c 	new file:   0x03-debugging/3-print_remaining_days.c 	modified:   0x03-debugging/main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (b < a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (a < b > c)
 	{
 		largest = b;
 	}
@@ -24,5 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
