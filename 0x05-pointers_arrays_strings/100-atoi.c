af72ae279855fb10166c5a09cf9976913601441f
Abogo <abogonono1@gmail.com>
2023-09-22 02:12:05 +0100

    A function that convert a string to number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - convert a string to number
@@ -29,14 +28,6 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		if (result > (INT_MAX - (s[i] - '0')) / 10)
-		{
-			if (sign == 1)
-				return (INT_MAX);
-			else
-				return (INT_MIN);
-		}
-
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
