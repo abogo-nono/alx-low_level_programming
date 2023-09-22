02482e4f38d74fba20cde537216658adf546b09b
Abogo <abogonono1@gmail.com>
2023-09-22 01:26:25 +0100

    updated version of header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 void print_array(int *a, int n);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 
 #endif /* MAIN_H */
