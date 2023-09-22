7ed640ebfee48fd0b7dca8d4e6b1d76599dd3c9c
Abogo <abogonono1@gmail.com>
2023-09-22 01:34:36 +0100

    A function that copy a string into another and return the destination string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,10 +8,10 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
+	int i;
 
-    for (i = 0; src[i] != '\0'; i++)
-        dest[i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-    return (dest);
+	return (dest);
 }
