235d468159f24239b882473335cc0695c5479db9
Abogo <abogonono1@gmail.com>
2023-09-23 02:55:09 +0100

    a function that encodes a string into 1337, correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,20 +7,27 @@
 */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j <= i; j++)
+		{
+			if (s[i] == 'a' || s[i] == 'A')
+				s[i] = '0' + 4;
+
+			else if (s[i] == 'e' || s[i] == 'E')
+				s[i] = '0' + 3;
+
+			else if (s[i] == 'o' || s[i] == 'O')
+				s[i] = '0' + 0;
+
+			else if (s[i] == 't' || s[i] == 'T')
+				s[i] = '0' + 7;
+
+			else if (s[i] == 'l' || s[i] == 'L')
+				s[i] = '0' + 1;
+		}
 	}
 
 	return (s);
