d7503967f8bfbe08645c070193be8af93fc49223
Abogo <abogonono1@gmail.com>
2023-09-23 03:21:36 +0100

    a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13.
+ * @s: th string passed
+ * Return: the encoded string
+*/
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		{
+			s[i] = s[i] + 13;
+		}
+		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+		{
+			s[i] = s[i] - 13;
+		}
+	}
+
+	return (s);
+
+}
