7d0fd901e3e1e9700449596ae734b9e6ed4a5f2a
Abogo <abogonono1@gmail.com>
2023-09-23 02:58:11 +0100

    a function that encodes a string into 1337, correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,19 +14,19 @@ char *leet(char *s)
 		for (j = 0; j <= i; j++)
 		{
 			if (s[i] == 'a' || s[i] == 'A')
-				s[i] = '0' + 4;
+				s[i] = '4';
 
 			else if (s[i] == 'e' || s[i] == 'E')
-				s[i] = '0' + 3;
+				s[i] = '3';
 
 			else if (s[i] == 'o' || s[i] == 'O')
-				s[i] = '0' + 0;
+				s[i] = '0';
 
 			else if (s[i] == 't' || s[i] == 'T')
-				s[i] = '0' + 7;
+				s[i] = '7';
 
 			else if (s[i] == 'l' || s[i] == 'L')
-				s[i] = '0' + 1;
+				s[i] = '1';
 		}
 	}
 
