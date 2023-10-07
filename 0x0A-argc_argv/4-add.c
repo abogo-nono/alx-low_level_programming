ecd3722a2c80586359a1a2032a2265515d6ec62d
Abogo <abogonono1@gmail.com>
2023-10-08 00:14:05 +0100

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	(void)argv;
 
 	for (i = 1; i < argc; i++)
-		if (atoi(argv[i]))
+		if (atoi(argv[i]) && atoi(argv[i]) >= 0)
 			result += atoi(argv[i]);
 		else
 		{
