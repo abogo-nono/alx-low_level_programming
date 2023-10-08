6ce2483cfdf8a590a50756e50eedd2265bd1165b
Abogo <abogonono1@gmail.com>
2023-10-08 12:12:48 +0100

    a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,94 +2,65 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: number arguments passed to gcc
- * @argv: table of passed arguments
- * Return: (0)
+ * minCoins - Calculate the minimum number of coins needed to make change.
+ * @amount: The amount for which change is to be made.
+ *
+ * Return: The minimum number of coins needed.
  */
+int minCoins(int amount)
+{
+	int coins[5] = {25, 10, 5, 2, 1};
+	int dp[1001];
+	int i, j;
+
+	if (amount <= 0)
+		return (0);
+
+	for (i = 0; i <= amount; i++)
+	{
+		dp[i] = amount + 1;
+	}
+
+	dp[0] = 0;
+
+	for (i = 1; i <= amount; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (i >= coins[j])
+			{
+				dp[i] = (dp[i] < (dp[i - coins[j]] + 1)) ? dp[i] : (dp[i - coins[j]] + 1);
+			}
+		}
+	}
+
+	return (dp[amount]);
+}
+
+/**
+* main - Entry point
+* @argc: The number of arguments passed to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 on success, 1 on error.
+*/
 int main(int argc, char *argv[])
 {
-	int result = 0, rest = atoi(argv[1]);
-	(void)argv;
+	int result;
 
-	if (argc != 1)
+	if (argc == 1)
 	{
-		printf("0\n");	
+		printf("Error\n");
 		return (1);
 	}
 
-	if (rest >= 25)
+	if (atoi(argv[1]) < 0)
 	{
-		rest = rest - 25;
-		result += 1;
-		while (rest >= 25)
-		{
-			if (rest - 25 < 0)
-			{
-				break;
-			}
-			rest = rest - 25;
-			result += 1;
-		}
+		printf("0\n");
 	}
-	if (rest >= 10)
-	{
-		rest = rest - 10;
-		result += 1;
-		while (rest >= 10)
-		{
-			if (rest - 10 < 0)
-			{
-				break;
-			}
-			rest = rest - 10;
-			result += 1;
-		}
-	}
-	if (rest >= 5)
-	{
-		rest = rest - 5;
-		result += 1;
-		while (rest >= 5)
-		{
-			if (rest - 5 < 0)
-			{
-				break;
-			}
-			rest = rest - 5;
-			result += 1;
-		}
-	}
-	if (rest >= 2)
-	{
-		rest = rest - 2;
-		result += 1;
-		while (rest >= 2)
-		{
-			if (rest - 2 < 0)
-			{
-				break;
-			}
-			rest = rest - 2;
-			result += 1;
-		}
-	}
-	if (rest >= 1)
-	{
-		rest = rest - 1;
-		result += 1;
-		while (rest >= 1)
-		{
-			if (rest - 1 < 0)
-			{
-				break;
-			}
-			rest = rest - 1;
-			result += 1;
-		}
-	}
+
+	result = minCoins(atoi(argv[1]));
 	printf("%d\n", result);
 
 	return (0);
-
 }
