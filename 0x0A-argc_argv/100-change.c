19799fe8a85b6df92355b911b7b15a1e7fbed26a
Abogo <abogonono1@gmail.com>
2023-10-08 11:54:48 +0100

    a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,18 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int minCoins(int);
+/**
+ * minCoins - Calculate the minimum number of coins needed to make change.
+ * @amount: The amount for which change is to be made.
+ *
+ * Return: The minimum number of coins needed.
+ */
+int minCoins(int amount)
+{
+	int coins[5] = {25, 10, 5, 2, 1};
+	int dp[1001];
+	int i, j;
+
+	if (amount <= 0)
+		return (0);
+
+	for (i = 0; i <= amount; i++)
+	{
+		dp[i] = amount + 1;
+	}
+
+	dp[0] = 0;
+
+	for (i = 1; i <= amount; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (i >= coins[j])
+			{
+				dp[i] = (dp[i] < (dp[i - coins[j]] + 1)) ? dp[i] : (dp[i - coins[j]] + 1);
+			}
+		}
+	}
+
+	return (dp[amount]);
+}
 
 /**
- * main - Entry point
- * @argc: number arguments passed to gcc
- * @argv: table of passed arguments
- * Return: (0)
- */
+* main - Entry point
+* @argc: The number of arguments passed to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 on success, 1 on error.
+*/
 int main(int argc, char *argv[])
 {
 	int result;
-	(void)argv;
 
 	if (argc == 1)
 	{
@@ -20,7 +54,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	if (atoi(argv[1]) <= 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("0\n");
 	}
@@ -29,36 +63,4 @@ int main(int argc, char *argv[])
 	printf("%d\n", result);
 
 	return (0);
-
-}
-
-/**
- * minCoins - a function that prints the
- *            minimum number of coins to make change for an amount of money
- * @amount: the passed amount
- * Return: the min value
- */
-int minCoins(int amount)
-{
-	int coins[] = {25, 10, 5, 2, 1};
-	int min = -1;
-	int i;
-
-	if (amount <= 0)
-		return (0);
-
-	for (i = 0; i < 5; i++)
-	{
-		if (amount >= coins[i])
-		{
-			int current = minCoins(amount - coins[i]) + 1;
-
-			if (min == -1 || current < min)
-			{
-				min = current;
-			}
-		}
-	}
-
-	return (min);
 }
