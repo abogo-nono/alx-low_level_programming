c6c73cf08ef6a6b3d0ab86267b48b13e97ef93a9
Abogo <abogonono1@gmail.com>
2023-10-11 02:44:06 +0100

    code refactored"

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,21 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (!d->name && !d->age && !d->owner)
-		return;
 	if (d)
 	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		d->age ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
