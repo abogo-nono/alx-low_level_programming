b84deb9488592775b57af63cc4b4d98195f51bc9
Abogo <abogonono1@gmail.com>
2023-12-10 01:14:20 +0100

    a function that add a new node to a double linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,20 +8,24 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *list = head;
-	dlistint_t *node = malloc(sizeof(dlistint_t));
+	dlistint_t **list = head;
+	dlistint_t *node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (node == NULL)
-		return (NULL);
+	{
+		perror("Can't allocate memory!");
+		exit(EXIT_FAILURE);
+	}
 
-	node->n = n;
-
-	if (list == NULL)
+	if (*list == NULL)
 		return (node);
 
+	node->n = n;
 	node->prev = NULL;
-	node->next = list;
-	list->prev = node;
+	node->next = *list;
+	(*list)->prev = node;
 
-	return (list);
+	*list = node;
+
+	return (*list);
 }
