84199f61611ea154caf5e7b8bdb28ed5a2a4e44e
Abogo <abogonono1@gmail.com>
2023-12-10 01:34:22 +0100

    a function that add a new node to a double linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *node = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t *node = malloc(sizeof(dlistint_t));
 
 	if (node == NULL)
 		return (NULL);
