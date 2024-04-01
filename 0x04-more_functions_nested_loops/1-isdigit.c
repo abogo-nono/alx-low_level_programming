f3fe98aa4221f3188c825b8611e35216e1c52d45
Abogo <abogonono1@gmail.com>
2024-04-01 08:34:05 -0500

    1-isdigit.c fixed

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
-* _isdigit - This function is used to check if a char passed is a digit
-* @c: the char to check
-* Return: 1 if c is a digit
-*/
+ * _isdigit - This function is used to check if a char passed is a digit
+ * @c: the char to check
+ * Return: 1 if c is a digit
+ */
 int _isdigit(int c)
 {
 	int i;
 
-	for (i = '0'; i <= '0' ; i++)
-	{
-		if (c == i)
-			return (1);
-	}
+	if ('0' <= c && c <= '9')
+		return (1);
+
 	return (0);
 }
