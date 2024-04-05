db2dd8afd70627ad39e4dd04262bc7d9c539a72a
Abogo <abogonono1@gmail.com>
2024-04-05 23:38:34 +0100

    print_binary fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,45 @@
 #include "main.h"
 
+/**
+ * get_len - get binary length of an integer
+ * @n: the integer
+ * Return: the length of the integer
+ */
+
+int get_len(unsigned long int n)
+{
+	int i = 0;
+
+	while (n)
+	{
+		i++;
+		n >>= 1;
+	}
+	return (i);
+}
 
 /**
  * print_binary -  a function that prints the binary representation of a number.
  *
  * @n: the number to print
  */
+
 void print_binary(unsigned long int n)
 {
-	int i = 0;
-	while (n!= 0)
-	{
-		_putchar(n % 2 + '0');
-		n /= 2;
-		i++;
-	}
-	while (i < 32)
-	{
-		_putchar('0');
-		i++;
-	}
-	_putchar('\n');
+	int i;
 
-	return;
+	if (n == 0)
+		_putchar('0');
+
+	for (i = get_len(n) - 1; i >= 0; i--)
+	{
+		if (n & (1 << i))
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
 }
