f4551c24e4c0732cfaa2c450b62504aaff4aee3d
Abogo <abogonono1@gmail.com>
2024-04-06 00:17:09 +0100

    0-read_textfile.c file fixed 	modified:   0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,10 +3,8 @@
 /**
  * read_textfile - function that reads a text file
  * and prints it to the POSIX standard output
- *
  * @filename: the name of the file to be read
  * @letters: is the number of letters it should read and print
- *
  * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -30,13 +28,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		fclose(file);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	if (bytes_written != bytes_read)
 		return (0);
-
-
 	free(buffer);
 	fclose(file);
 
