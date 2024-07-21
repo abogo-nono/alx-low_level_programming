e45149854390e11e4510062508827216e37d227c
Abogo <abogonono1@gmail.com>
2024-07-21 02:24:42 +0100

    style error fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,14 @@
 #include "search_algos.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 /**
- * linear_search - that searches for a value in an array of integers using
+ * linear_search - that searches for a value in
+ * an array of integers using
  * the Linear search algorithm
  *
- * @array: a pointer to the first element of the array we search in
+ * @array: a pointer to the first element of
+ * the array we search in
  * @size: the size of the array we search in.
  * @value: the value we search in the array
  *
@@ -16,17 +17,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	size_t i;
 
 	if (!array || size == 0)
 		return (-1);
 
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
 		if (array[i] == value)
 			return (i);
-		i++;
 	}
 
 	return (-1);
